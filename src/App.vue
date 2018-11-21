1e09592891268c56d97692c52e5097606dfbfed5
AshleyAbramowiczGibbs <ashleyabramo2@gmail.com>
2018-11-20 20:46:17 -0600

    Show blog data in frontend

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="#">My blog App</a>
+
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/signup">Signup</router-link>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/#/login">Login</a>
+          </li>
+            <li class="nav-item">
+            <a class="nav-link" href="/#/logout">Logout</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
     <router-view/>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+      <div class="row">
+        <div v-for="post in posts" class="col-md-4 mb-2">
+          <div class="card">
+
+            <div class="card-body">
+              <h5 class="card-title">{{ post.title }}</h5>
+              <p class="card-text">body: {{ post.body }}</p>
+
+            </div>
+              <a v-bind:href="'/#/posts/' + post.id" class="btn btn-primary">Show blog</a>
+          </div>
+
+        </div>
+      </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
+  let axios = require("axios");
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+    data: function() {
+      return {
+        message: "Welcome to Vue.js!",
+        posts: [],
+        title: "",
+        body: ""
+      };
+    },
+    created: function() {
+      axios.get("http://localhost:3000/api/posts").then(
+        function(response) {
+          console.log(response.data);
+          this.posts = response.data;
+        }.bind(this)
+      );
+    },
+    methods: {},
+    computed: {}
+};
 </script>
